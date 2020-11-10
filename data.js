88101bae58e40025c719b90bad6c165f64bded6c
Thanakit <thanakittinop05@hotmail.com>
2020-11-10 23:28:59 +0700

    ser Commit

---
+++ data.js
@@ -0,0 +1,25 @@
+const data = {
+    "Aboutme": {
+        "FullName": "Thanakit Tinop",
+        "Nickname": "Tle",
+        "Birth": "Monday, 7 February 2000 - 21 Year",
+        "Phone": "0659216799",
+        "Email": "614259034@webmail.npru.ac.th",
+        "Website": "www.tzs-global.com",
+        "Work": "Student",
+        "Address": "81 M.4 Thachumpon Photharam Ratchaburi 70120"
+    },
+    "Skill": {
+        "HTML": "80%",
+        "PHP": "50%",
+        "CSS": "70%",
+        "JavaScript": "20%",
+        "Java": "20%"
+    },
+    "Education": {
+        "Junior": "Phothawatthanasenee School, Photharam Ratchaburi",
+        "High": "Phothawatthanasenee School, Photharam Ratchaburi",
+        "Present" : "Nakhon Pathom Rajabhat University, Faculty of Science and Technology, Software Engineering"
+    }
+}
+module.exports = data;
